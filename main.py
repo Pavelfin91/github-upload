5daa742967e98711fa584328d60263640a3837d2
pavel <pavelfinkelbaum@gmail.com>
2021-12-15 18:18:50 +0200

    Wix_bi

--- main.py
+++ main.py
@@ -4,30 +4,38 @@ import pandas as pd
 from sqlalchemy import create_engine
 import auth
 
+# First Stage
 
+# Sending a GET request to the mentioned URL retrieving 4500 records
+# then, parse the string representation of JSON file to rows of tabular form
 res = requests.get("https://randomuser.me/api/?results=4500")
 if res.ok:
-    dataset = pd.json_normalize(json.loads(res.text), record_path=['results'])
+    main_dataset = pd.json_normalize(json.loads(res.text), record_path=['results'])
 
+# Establishing connection to the database
+engine = create_engine('mysql+pymysql://' + str(auth.user) + ':' + str(auth.pw) + '@' + str(auth.host) + ':'
+                       + str(auth.port) + '/' + str(auth.database))
 
-engine = create_engine('mysql+pymysql://'+str(auth.user)+':'+str(auth.pw)+'@'+str(auth.host)+':'
-+str(auth.port)+'/'+str(auth.database))
-#                        .format(host=auth.host, db=auth.database, user=auth.user, pw=auth.password))
+# connect to MYSQLDB
 conn = engine.connect()
 
-male_dataset = dataset.query("gender == 'male'")
-male_dataset.to_sql(con=conn, name='PAVEL_test_male', if_exists='replace', index=False)
+# Second Stage
 
-female_dataset = dataset.query("gender == 'female'")
+# creating new datasets via queries and inserting them to DB
+male_dataset = main_dataset.query("gender == 'male'")
+female_dataset = main_dataset.query("gender == 'female'")
+
+male_dataset.to_sql(con=conn, name='PAVEL_test_male', if_exists='replace', index=False)
 female_dataset.to_sql(con=conn, name='PAVEL_test_male', if_exists='replace', index=False)
 
-# max_age_df = dataset['dob.age']
-# print(max_age_df.max())
-# max_age = int(round(max_age_df.max())/10)
+# Third Stage
 
+# create the dob.age-subsets and insert the resulting dataframes to DB
 for i in range(10):
-    age_subset = dataset[((i+1) * 10 <= dataset['dob.age']) & (dataset['dob.age'] < (i + 2) * 10)]
-    age_subset.to_sql(con=conn, name='PAVEL_test_'+str(i+1), if_exists='replace', index=False)
+    age_subset = main_dataset[((i + 1) * 10 <= main_dataset['dob.age']) & (main_dataset['dob.age'] < (i + 2) * 10)]
+    age_subset.to_sql(con=conn, name='PAVEL_test_' + str(i + 1), if_exists='replace', index=False)
+
+# Fourth Stage
 
 last_reg_sql = """\
                (SELECT *
@@ -40,9 +48,12 @@ last_reg_sql = """\
                ORDER BY 'registered.date' DESC
                LIMIT 20)"""
 
+# querying the DB
 last_reg = pd.read_sql_query(last_reg_sql, engine)
+# inserting DF to DB
 last_reg.to_sql(con=conn, name='PAVEL_test_20', if_exists='replace', index=False)
 
+# Fifth Stage
 
 twenty_join_five_sql = """\
                            (SELECT *
@@ -51,20 +62,19 @@ twenty_join_five_sql = """\
                            (SELECT *
                            FROM PAVEL_test_5)"""
 
+# querying the DB and creating JSON from dataframe
 twenty_join_five = pd.read_sql_query(twenty_join_five_sql, engine)
 twenty_join_five.to_json(path_or_buf="C:/Users/pavel/PycharmProjects/Wix_Bi/first.json")
 
+# Sixth Stage
 
 twenty_join_two_sql = """\
                            (SELECT *
                            FROM PAVEL_test_20)
                            UNION ALL
                            (SELECT *
-                           FROM PAVEL_test_5)"""
+                           FROM PAVEL_test_2)"""
 
+# querying the DB and creating JSON from dataframe
 twenty_join_two = pd.read_sql_query(twenty_join_two_sql, engine)
 twenty_join_two.to_json(path_or_buf="C:/Users/pavel/PycharmProjects/Wix_Bi/second.json")
-
-
-# mycursor = mydb.cursor()
-# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
